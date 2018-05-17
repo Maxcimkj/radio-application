b565f5f767aee8249ff3cd681f25f1e81cf6fb4c
maxcimkj <maxcimkj93@gmail.com>
2018-05-17 23:32:52 +0700

    Create base dirty version

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
-
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {GenreService} from "./genre.service";
 import {HttpClientModule} from "@angular/common/http";
 import {HttpClientInMemoryWebApiModule} from "angular-in-memory-web-api";
 import {InMemoryGenreDataService} from "./in-memory-genre-data.service";
-import { MessageService } from './message.service';
+import {MessageService} from './message.service';
 import {MusicModule} from "./music/music.module";
 
 @NgModule({
@@ -19,9 +20,12 @@ import {MusicModule} from "./music/music.module";
     HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(
       InMemoryGenreDataService, {dataEncapsulation: false}),
-    MusicModule
+    MusicModule,
+    NgbModule.forRoot(),
+    FormsModule
   ],
   providers: [GenreService, MessageService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
