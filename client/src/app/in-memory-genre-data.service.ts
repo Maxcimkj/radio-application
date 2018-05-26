e2d2003ee3468b4454a2ae52677d47f89f78f99a
maxcimkj <maxcimkj93@gmail.com>
2018-05-26 17:21:24 +0700

    Changed field names in memory data service

--- client/src/app/in-memory-genre-data.service.ts
+++ client/src/app/in-memory-genre-data.service.ts
@@ -16,37 +16,37 @@ export class InMemoryGenreDataService implements InMemoryDbService {
     const tracks = [
       {
         id: 1, name: 'Beautiful Lie', artist: 'Hans Zimmer1', release: 'Batman V Superman: Dawn Of Justice',
-        duration: 227, style : 'rock',
+        duration: 227, style: 'rock',
         cover: 'http://onetwo.tv/storage/images/covers/250x250/0/0/4/67/ef6219bv4rkg.jpg',
         url: 'http://onetwo.tv/storage/media/releases/0/0/4/67/olq92p6id34w/01%20-%20Hans%20Zimmer%20And%20Junkie%20XL%20-%20Beautiful%20Lie.mp3'
       },
       {
         id: 2, name: 'Their War Here', artist: 'Hans Zimmer1', release: 'Batman V Superman: Dawn Of Justice',
-        duration: 276, style : 'rock',
+        duration: 276, style: 'rock',
         cover: 'http://onetwo.tv/storage/images/covers/250x250/0/0/4/67/ef6219bv4rkg.jpg',
         url: 'http://onetwo.tv/storage/media/releases/0/0/4/67/olq92p6id34w/02%20-%20Hans%20Zimmer%20And%20Junkie%20XL%20-%20Their%20War%20Here.mp3'
       },
       {
         id: 3, name: 'The Red Capes Are Coming', artist: 'Hans Zimmer1', release: 'Batman V Superman: Dawn Of Justice',
-        duration: 212, style : 'rock',
+        duration: 212, style: 'rock',
         cover: 'http://onetwo.tv/storage/images/covers/250x250/0/0/4/67/ef6219bv4rkg.jpg',
         url: 'http://onetwo.tv/storage/media/releases/0/0/4/67/olq92p6id34w/03%20-%20Hans%20Zimmer%20And%20Junkie%20XL%20-%20The%20Red%20Capes%20Are%20Coming.mp3'
       },
       {
         id: 4, name: 'Day Of The Dead', artist: 'Hans Zimmer2', release: 'Batman V Superman: Dawn Of Justice',
-        duration: 242, style : 'metal',
+        duration: 242, style: 'metal',
         cover: 'http://onetwo.tv/storage/images/covers/250x250/0/0/4/67/ef6219bv4rkg.jpg',
         url: 'http://onetwo.tv/storage/media/releases/0/0/4/67/olq92p6id34w/04%20-%20Hans%20Zimmer%20And%20Junkie%20XL%20-%20Day%20Of%20The%20Dead.mp3'
       },
       {
         id: 5, name: 'Must There Be A Superman?', artist: 'Hans Zimmer2', release: 'Batman V Superman: Dawn Of Justice',
-        duration: 243, style : 'rock',
+        duration: 243, style: 'rock',
         cover: 'http://onetwo.tv/storage/images/covers/250x250/0/0/4/67/ef6219bv4rkg.jpg',
         url: 'http://onetwo.tv/storage/media/releases/0/0/4/67/olq92p6id34w/05%20-%20Hans%20Zimmer%20And%20Junkie%20XL%20-%20Must%20There%20Be%20A%20Superman_.mp3'
       },
       {
         id: 6, name: 'New Rules', artist: 'Hans Zimmer2', release: 'Batman V Superman: Dawn Of Justice',
-        duration: 244, style : 'metal',
+        duration: 244, style: 'metal',
         cover: 'http://onetwo.tv/storage/images/covers/250x250/0/0/4/67/ef6219bv4rkg.jpg',
         url: 'http://onetwo.tv/storage/media/releases/0/0/4/67/olq92p6id34w/06%20-%20Hans%20Zimmer%20And%20Junkie%20XL%20-%20New%20Rules.mp3'
       },
